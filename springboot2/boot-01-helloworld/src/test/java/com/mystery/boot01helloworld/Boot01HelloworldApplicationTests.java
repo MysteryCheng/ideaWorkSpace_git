ffc0215cfe3452220988d87bfb40634cd893ef43
mystery <434892446@qq.com>
2022-08-18 17:25:51 +0800

    boot01

---
+++ springboot2/boot-01-helloworld/src/test/java/com/mystery/boot01helloworld/Boot01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystery.boot01helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
